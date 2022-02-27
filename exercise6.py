77241fe2b00431d25315f187644a704c2c464ad8
Farbod-BM <100041740+Farbod-BM@users.noreply.github.com>
2022-02-28 00:57:10 +0330

    Add files via upload

---
+++ exercise6.py
@@ -0,0 +1,20 @@
+x = 2
+
+print("x")
+# It will print x as a str
+
+print('x')
+# It will print  x as a Str
+
+print(x)
+# It's going to print the value of variable, so it will show 2
+
+print("x+1")
+# The whole expression between "" is an str, so it will print x+1
+
+# print('x'+1)
+# an error will occur because a math operation is happening in string
+
+print(x+1)
+# This one is a simple math operation and the result is 3
+
