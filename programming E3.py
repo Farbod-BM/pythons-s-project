2f52aae7addec95c9f764fa6265862440e762955
Farbod-BM <100041740+Farbod-BM@users.noreply.github.com>
2022-03-06 22:59:55 +0330

    Add files via upload

---
+++ programming E3.py
@@ -0,0 +1,13 @@
+input1 = int(input(" enter a number: "))
+input2 = int(input(" enter a number: "))
+input3 = int(input(" enter a number: "))
+input4 = int(input(" enter a number: "))
+input5 = int(input(" enter a number: "))
+
+if input1 == input2 or input1 == input3 or input1 == input4 or input1 == input5:
+    print("Duplicate")
+elif input2 == input3 or input2 == input4 or input2 == input5 or input3 == input4 or input3 ==input5 or input4 == input5:
+    print("Duplicate")
+else:
+    print("All unique")
+
