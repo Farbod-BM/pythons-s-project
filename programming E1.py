f4044ff8d2033148921bc06f3fdfeeb63f452e75
Farbod-BM <100041740+Farbod-BM@users.noreply.github.com>
2022-03-06 22:57:06 +0330

    Add files via upload

---
+++ programming E1.py
@@ -0,0 +1,28 @@
+income = int(input("Please enter your income: "))
+
+if 0 < income < 1000:
+    afterReduction = income
+    print("net income after tax reduction is", end=" ")
+    print(afterReduction)
+elif 1000 < income <= 2500:
+    afterReduction = income - (income * 0.1)
+    print("net income after tax reduction is", end=" ")
+    print(afterReduction)
+elif 2500 < income <= 4000:
+    afterReduction = income - (income * 0.15)
+    print("net income after tax reduction is", end=" ")
+    print(afterReduction)
+elif 4000 < income <= 6000:
+    afterReduction = income - (income * 0.2)
+    print("net income after tax reduction is", end=" ")
+    print(afterReduction)
+elif 6000 < income <= 8000:
+    afterReduction = income - (income * 0.25)
+    print("net income after tax reduction is", end=" ")
+    print(afterReduction)
+elif  income < 8000:
+    afterReduction = income - (income * 0.3)
+    print("net income after tax reduction is", end=" ")
+    print(afterReduction)
+else:
+    print("error")
